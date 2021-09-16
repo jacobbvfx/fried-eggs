1b401cfa615c7881029f5a8ad8c3747897e0ba6c
Jacob <38792138+shadoweG@users.noreply.github.com>
2021-09-16 20:34:22 +0200

    yummy commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# fried-eggs
+Best bash script **EVER**, was I drunk then? Maybe!
+
+## Installation
+~~Install **GNU/Linux** cuz I don't know why you didn't did it already~~<br>
+Just clone the repo and run by typing ./fried-eggs (and word you want to say if you really want to because you dont have to but you can)
+
+## TODO:
+* Add bacon
+* Make a version for ~~***wINbLOaTs***~~
+
+## Preview
+typing `git clone https://github.com/shadoweG/fried-eggs.git` isn't that hard
---
+++ fried-eggs.sh
@@ -0,0 +1,19 @@
+if [$@ = ""]; then 
+	echo "it's fried eggs time!"
+else
+	echo $@
+fi
+
+echo $'	'
+echo $'        . -- ~~~ -- .'
+echo $'    .-~               ~-.'
+echo $'   /                     \\'
+echo $'  /      **               \              _'
+echo $' |      *  *               |____________| |'
+echo $' |       **                                \\'
+echo $' |           ***           _____________   /'
+echo $'  \         *   *         /             |_|'
+echo $'   \        *   *        /'
+echo $'    `-.      ***      .-'\'
+echo $'        ~- . ___ . -~'
+echo $'	'
